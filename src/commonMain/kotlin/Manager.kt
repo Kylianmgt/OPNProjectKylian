49e6a937bd2d02dcc9aaa2c4758246322ad0f9ef
Kylianmgt <kylian.migot@viacesi.fr>
2021-02-18 21:42:10 +0100

    Game over on collision, bug the game wont restart on space bar pressed

--- src/commonMain/kotlin/Manager.kt
+++ src/commonMain/kotlin/Manager.kt
@@ -1,5 +1,8 @@
 import com.soywiz.klock.TimeSpan
 import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.Text
+import com.soywiz.korge.view.centerOnStage
+import com.soywiz.korge.view.text
 import com.soywiz.korio.async.delay
 
 class Manager (private val container : Container){
@@ -13,6 +16,8 @@ class Manager (private val container : Container){
 
     var isRunning = false
     var status = GameStatus.NOT_STARTED
+    var message: Text? = null
+    var gameOverMessage: Text? = null
 
     fun start(){
         isRunning = true
@@ -23,13 +28,37 @@ class Manager (private val container : Container){
     fun finish(){
         isRunning = false
         status = GameStatus.FINISHED
+        if(message == null && gameOverMessage == null) {
+            gameOverPrint()
+        }
 
     }
     suspend fun restart(){
+        removeMessage()
         status = GameStatus.RESTART
         delay(TimeSpan(500.0))
         start()
     }
 
+    private fun gameOverPrint() {
+        val gameOverContainer = container.text("GAME OVER")
+        gameOverContainer.centerOnStage()
+        gameOverContainer.y += 50
+        val text = container.text("Press Space to restart")
+        text.centerOnStage()
+        text.y += 100
+        container.addChild(text)
+        container.addChild(gameOverContainer)
+        message = text
+        gameOverMessage = gameOverContainer
+    }
 
+    private fun removeMessage() {
+        container.removeChild(gameOverMessage)
+        container.removeChild(message)
+        message = null
+        gameOverMessage = null
+    }
 }
+
+
